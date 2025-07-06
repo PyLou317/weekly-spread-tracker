f0efc822d4457e46ec1423f48116ef82510e7d8b
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-07-06 10:50:20 -0700

    Update clients app with new migrations and model improvements

--- clients/models.py
+++ clients/models.py
@@ -1,10 +1,9 @@
-
 from django.db import models
 from django.contrib.auth.models import User
 
 
 class Client(models.Model):
-    name = models.CharField(max_length=200, unique=True)
+    name = models.CharField(max_length=200)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
